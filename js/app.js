fff8def620e6eda28720f51274c0a2abe5eb10b1
CIA <cgberlin>
2016-07-08 14:40:43 -0500

    moved price location

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
 	var addedItem = $('.item-to-add').val();
 	var price = $('.estimated-price').val();
 
-	var listString = '<li><div class="list-item"><p class="checkbox"><input type="checkbox">Got item</p><p>' + addedItem +' | $' + price + '</p><button class="remove-item">Remove</button></div></li>'
+	var listString = '<li><div class="list-item"><p class="checkbox"><input type="checkbox">Got item</p><p>' + addedItem +'</p><button class="remove-item">Remove</button><p>$'+price+'</p></div></li>'
 
 	$('.add-item-button').click(function(){
 		$('ul').append(listString);
