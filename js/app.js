cf3fd8890ed806131e72c4ebde3bbc443ba5a5b3
CIA <cgberlin>
2016-07-08 17:05:46 -0500

    added event handlers for individual remove buttons

--- js/app.js
+++ js/app.js
@@ -2,8 +2,16 @@ $(document).ready(function(){
 
 	var addedItem = $('.item-to-add').val();
 	var price = $('.estimated-price').val();
-
+	
 	var listString = '<li><div class="list-item"><p class="checkbox"><input type="checkbox">Got item</p><p>' + addedItem +'</p><button class="remove-item">Remove</button><p>$'+price+'</p></div></li>'
+	var totalPrice 
+
+
+	$('.main').on('click', '.remove-item', function(){
+		var removeThis = $(this).closest('.list-item');
+		removeThis.remove();
+	});
+
 
 	$('.add-item-button').click(function(){
 		$('ul').append(listString);
@@ -11,9 +19,9 @@ $(document).ready(function(){
 
 	$('.reset-list').click(function(){
 		$('li').remove();
-
 	});
 
+	
 
 
 
