bb893b26f9e0f3f27af4c70cfade18a18cc41abb
CIA <cgberlin>
2016-07-08 14:09:10 -0500

    added listeners to the add item button.

---
+++ js/app.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+
+	var addItemValue = $('.item-to-add').val();
+	var price = $('.estimated-price').val();
+
+
+	$('.add-item-button').click(function(){
+		alert(addItemValue + price);
+	});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
