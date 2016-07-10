68ec04e51942177aceda0034cf49e1fed350f54e
cgberlin <codyberlin@gmail.com>
2016-07-10 13:44:11 -0700

    added running total

--- js/app.js
+++ js/app.js
@@ -1,14 +1,19 @@
 $(document).ready(function(){
+
+	var totalPrice = 0
+
 	$('.main').on('click', '.remove-item', function(){
 		var removeThis = $(this).closest('.list-item');
 		removeThis.remove();
 	});
 	$('.add-item-button').click(function(){
 		var addedItem = $('.item-to-add').val();
-		var price = $('.estimated-price').val();
+		var price = parseInt($('.estimated-price').val(),10);
+		totalPrice += price;
 		var listString = '<li><div class="list-item"><p class="checkbox"><input type="checkbox">Got item</p><p class="item-name">' + addedItem +'</p><button class="remove-item">Remove</button><p>$'+price+'</p></div></li>'
 		$('ul').append(listString);
-	});
+		$('.price-total > p').replaceWith('<p>Total Price: $'+totalPrice+'</p>');
+			});
 
 	$('.reset-list').click(function(){
 		$('li').remove();
