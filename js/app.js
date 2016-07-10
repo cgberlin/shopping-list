278e068f12f26f040eece26c72ce41ba78a53cfa
cgberlin <codyberlin@gmail.com>
2016-07-10 12:52:21 -0700

    added full funcionality to the add item and price buttons and text areas

--- js/app.js
+++ js/app.js
@@ -1,19 +1,12 @@
 $(document).ready(function(){
-
-	var addedItem = $('.item-to-add').val();
-	var price = $('.estimated-price').val();
-	
-	var listString = '<li><div class="list-item"><p class="checkbox"><input type="checkbox">Got item</p><p class="item-name">' + addedItem +'</p><button class="remove-item">Remove</button><p>$'+price+'</p></div></li>'
-	var totalPrice 
-
-
 	$('.main').on('click', '.remove-item', function(){
 		var removeThis = $(this).closest('.list-item');
 		removeThis.remove();
 	});
-
-
 	$('.add-item-button').click(function(){
+		var addedItem = $('.item-to-add').val();
+		var price = $('.estimated-price').val();
+		var listString = '<li><div class="list-item"><p class="checkbox"><input type="checkbox">Got item</p><p class="item-name">' + addedItem +'</p><button class="remove-item">Remove</button><p>$'+price+'</p></div></li>'
 		$('ul').append(listString);
 	});
 
