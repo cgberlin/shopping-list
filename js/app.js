4456e0b1c5e3ef1da1c2822ec0e6bac3e1dbb202
cgberlin <codyberlin@gmail.com>
2016-07-10 14:27:26 -0700

    fixed orientation issues

--- js/app.js
+++ js/app.js
@@ -4,19 +4,25 @@ $(document).ready(function(){
 
 	$('.main').on('click', '.remove-item', function(){
 		var removeThis = $(this).closest('.list-item');
-		removeThis.remove();
+		$(removeThis).fadeOut(1500);
+		$(removeThis).delay(1500).remove();
 	});
 	$('.add-item-button').click(function(){
 		var addedItem = $('.item-to-add').val();
 		var price = parseInt($('.estimated-price').val(),10);
 		totalPrice += price;
-		var listString = '<li><div class="list-item"><p class="checkbox"><input type="checkbox">Got item</p><p class="item-name">' + addedItem +'</p><button class="remove-item">Remove</button><p>$'+price+'</p></div></li>'
+		var listString = '<li><div class="list-item"><p class="checkbox"><input type="checkbox">Got item</p><p class="item-name">' + addedItem +'</p><button class="remove-item">Remove</button><p class="the-price">$'+price+'</p></div></li>'
 		$('ul').append(listString);
+		$('.list-item:last').hide().fadeIn(1500);
 		$('.price-total > p').replaceWith('<p>Total Price: $'+totalPrice+'</p>');
 			});
 
 	$('.reset-list').click(function(){
-		$('li').remove();
+		$('li').fadeOut(1000, function(){
+			$(this).remove();
+			totalPrice = 0
+			$('.price-total > p').replaceWith('<p>Total Price: $'+totalPrice+'</p>');
+		});
 	});
 
 	$('.main').on('change', 'input[type=checkbox]', function() {
