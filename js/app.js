356d9841ed480ba7984e665be23992c50c358bf1
CIA <cgberlin>
2016-07-09 19:34:33 -0500

    added some event handlers to checkboxes

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
 	var addedItem = $('.item-to-add').val();
 	var price = $('.estimated-price').val();
 	
-	var listString = '<li><div class="list-item"><p class="checkbox"><input type="checkbox">Got item</p><p>' + addedItem +'</p><button class="remove-item">Remove</button><p>$'+price+'</p></div></li>'
+	var listString = '<li><div class="list-item"><p class="checkbox"><input type="checkbox">Got item</p><p class="item-name">' + addedItem +'</p><button class="remove-item">Remove</button><p>$'+price+'</p></div></li>'
 	var totalPrice 
 
 
@@ -19,6 +19,10 @@ $(document).ready(function(){
 
 	$('.reset-list').click(function(){
 		$('li').remove();
+	});
+
+	$('input[type=checkbox]').on('change', function() {
+		$(this).parent('.checkbox').siblings('.item-name').css('text-decoration', 'line-through').css('color', 'transparent').css('text-shadow', '0 0 5px rgba(0,0,0,0.5)');
 	});
 
 	
