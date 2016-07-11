811901e376cc53f6a429c8fa44697b85e92d1ea9
CIA <cgberlin>
2016-07-11 15:26:47 -0500

    cleaned up everything, made css and js DRYer, fixed some design and resizing stuff

--- js/app.js
+++ js/app.js
@@ -1,66 +1,66 @@
 $(document).ready(function(){
 
-	var totalPrice = 0
-
+	function replaceTotal() {
+		$('.price-total > p').replaceWith('<p>Total Price: $'+totalPrice+'</p>');
+	}
+	function removeThis() {
+		$(this).remove();
+	}
+	function addString(listString) {
+		$('ul').append(listString);
+		$('.list-item:last').hide().fadeIn(1500);
+		replaceTotal();
+	}
+	var itemToAdd = $('.item-to-add');
+	var totalPrice = 0;
 	$('.main').on('click', '.remove-item', function(){
 		var priceRemove = $(this).siblings('.the-price').text();
 		var removePrice = parseInt(priceRemove, 10);
 		totalPrice -= removePrice;
-		$('.price-total > p').replaceWith('<p>Total Price: $'+totalPrice+'</p>');
-		var removeThis = $(this).closest('.list-item');
-		$(removeThis).fadeOut(1000, function(){
-			$(this).remove();
+		replaceTotal();
+		var removeClosestItem = $(this).closest('.list-item');
+		$(removeClosestItem).fadeOut(1000, function(){
+			removeThis();
 		});
-		
-		
 	});
 	$('.add-item-button').click(function(){
-		if ($('.item-to-add').val() == 0) {
-			alert('Please enter an item');
+		if ($(itemToAdd).val() == 0) {
+			$(itemToAdd).val('Please Enter Item');
 		}
 		else{
-		var addedItem = $('.item-to-add').val();
-		var price = parseInt($('.estimated-price').val(),10);
-		totalPrice += price;
-		var listString = '<li><div class="list-item"><p class="checkbox"><input type="checkbox">Got item</p><p class="item-name">' + addedItem +'</p><button class="remove-item">Remove</button><p class="the-price">'+price+'</p></div></li>'
-		$('ul').append(listString);
-		$('.list-item:last').hide().fadeIn(1500);
-		$('.price-total > p').replaceWith('<p>Total Price: $'+totalPrice+'</p>');
+			var addedItem = $(itemToAdd).val();
+			var price = parseInt($('.estimated-price').val(),10);
+			if (price == 0){
+			totalPrice = totalPrice;
+			var listString = '<li class="inserted-row"><div class="list-item"><p class="checkbox"><input type="checkbox">Got item</p><p class="item-name">' + addedItem +'</p><button class="remove-item">Remove</button><p class="the-price" style="display:none;">0</p></div></li>';
+			addString(listString);
+			}
+			else {
+			totalPrice += price;
+			var listString = '<li class="inserted-row"><div class="list-item"><p class="checkbox"><input type="checkbox">Got item</p><p class="item-name">' + addedItem +'</p><button class="remove-item">Remove</button><p class="the-price">'+price+'</p></div></li>'
+			addString(listString);
+		}
 	}
 			});
-
-
 	$('.reset-list').click(function(){
-		$('li').fadeOut(1000, function(){
-			$(this).remove();
-			totalPrice = 0
-			$('.price-total > p').replaceWith('<p>Total Price: $'+totalPrice+'</p>');
+		$('.inserted-row').fadeOut(1000, function(){
+			removeThis();
+			totalPrice = 0;
+			replaceTotal();
 		});
 	});
-
 	$('.main').on('change', 'input[type=checkbox]', function() {
 		if ($(this).prop('checked')==true){
-			$(this).parent('.checkbox').siblings('.item-name').css('text-decoration', 'line-through').css('color', 'transparent').css('text-shadow', '0 0 5px rgba(0,0,0,0.5)');
+			$(this).parent('.checkbox').siblings('.item-name')
+			.css('text-decoration', 'line-through')
+			.css('color', 'transparent')
+			.css('text-shadow', '0 0 5px rgba(0,0,0,0.5)');
 						}
 		else { 
-			$(this).parent('.checkbox').siblings('.item-name').css('text-decoration', 'none').css('color', 'black').css('text-shadow', 'none');
+			$(this).parent('.checkbox').siblings('.item-name')
+			.css('text-decoration', 'none')
+			.css('color', 'black')
+			.css('text-shadow', 'none');
 		}
 	});
-
-	
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 });
